32654abe86d9251acc33f62a06e89f1f0285752a
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-12-14 21:13:39 +0100

    adds parent-child component communication and data flow of properties and events

--- src/app/components/rooms/rooms.component.ts
+++ src/app/components/rooms/rooms.component.ts
@@ -15,45 +15,51 @@ export class RoomsComponent implements OnInit {
     availableRooms: 32,
     bookedRooms: 23
   };
-  roomList: RoomList[] = [
-    {
-      roomType: 'Deluxe Room',
-      roomNumber: 1,
-      amenities: 'Air Condition, Free Wi-Fi, Kitchen, Bathroom',
-      price: 999,
-      photos: '/rooms/room-1.jpg',
-      checkinTime: new Date('11-Nov-2022'),
-      checkoutTime: new Date('11-Dez-2022'),
-      rating: 4.6252,
-    },
-    {
-      roomType: 'Normal Room',
-      roomNumber: 2,
-      amenities: 'Air Condition, Free Wi-Fi, Kitchen, Bathroom',
-      price: 470,
-      photos: '/rooms/room-2.jpg',
-      checkinTime: new Date('01-Sep-2022'),
-      checkoutTime: new Date('25-Okt-2022'),
-      rating: 4.245,
-    },
-    {
-      roomType: 'Limited Room',
-      roomNumber: 3,
-      amenities: 'Air Condition, Free Wi-Fi, Kitchen, Bathroom',
-      price: 640,
-      photos: '/rooms/room-3.jpg',
-      checkinTime: new Date('01-Jan-2022'),
-      checkoutTime: new Date('30-Dez-2022'),
-      rating: 4.8,
-    },
-  ]
+  roomList: RoomList[] = [];
+  selectedRoom!: RoomList;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.roomList = [
+      {
+        roomType: 'Deluxe Room',
+        roomNumber: 1,
+        amenities: 'Air Condition, Free Wi-Fi, Kitchen, Bathroom',
+        price: 999,
+        photos: '/rooms/room-1.jpg',
+        checkinTime: new Date('11-Nov-2022'),
+        checkoutTime: new Date('11-Dez-2022'),
+        rating: 4.6252,
+      },
+      {
+        roomType: 'Normal Room',
+        roomNumber: 2,
+        amenities: 'Air Condition, Free Wi-Fi, Kitchen, Bathroom',
+        price: 470,
+        photos: '/rooms/room-2.jpg',
+        checkinTime: new Date('01-Sep-2022'),
+        checkoutTime: new Date('25-Okt-2022'),
+        rating: 4.245,
+      },
+      {
+        roomType: 'Limited Room',
+        roomNumber: 3,
+        amenities: 'Air Condition, Free Wi-Fi, Kitchen, Bathroom',
+        price: 640,
+        photos: '/rooms/room-3.jpg',
+        checkinTime: new Date('01-Jan-2022'),
+        checkoutTime: new Date('30-Dez-2022'),
+        rating: 4.8,
+      },
+    ];
   }
 
   toggle(): void {
     this.hideRooms = !this.hideRooms;
+  }
+
+  selectRoom(room: RoomList) {
+    this.selectedRoom = room;
   }
 }
