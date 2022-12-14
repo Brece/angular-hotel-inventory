e2956a812d5ee30fa2c384977229733916c8e815
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-12-14 01:36:03 +0100

    adds ngx-bootstrap and bootstrap 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// import { setTheme } from 'ngx-bootstrap/utils';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angular-hotel-inventory';
   role = 'Admin';
+
+  constructor() {
+    // setTheme('bs5');
+  }
 }
